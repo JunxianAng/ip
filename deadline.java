427891785de36f0dd9a00404480a1bc95e91f61e
LAPTOP-POOFUI22\User <jun_xian@live.com>
2020-09-03 00:35:37 +0800

    Deadline

--- deadline.java
+++ deadline.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class deadline {
+public class deadline extends Task{
+
+    protected String byDate;
+
+    public deadline(String description, String byDate) {
+        super(description);
+        this.byDate = byDate;
+    }
+
+    public void printStatus(){
+        System.out.print("[D]");
+        super.printStatus();
+        System.out.println(" (by: " + byDate + ")" );
+    }
+
+    public void printAction(){
+        super.printAction();
+        printStatus();
+        System.out.println("Now you have "+ TaskCount + " tasks in the list.");
+    }
 }
