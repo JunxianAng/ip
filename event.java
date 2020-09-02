f54a2426a706708f89b4a91b7b00f2de0a9cc077
LAPTOP-POOFUI22\User <jun_xian@live.com>
2020-09-03 00:35:46 +0800

    Event

--- event.java
+++ event.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class event {
+public class event extends Task{
+
+    protected String byDate;
+
+    public event(String description, String byDate) {
+        super(description);
+        this.byDate = byDate;
+    }
+
+    public void printStatus(){
+        System.out.print("[E]");
+        super.printStatus();
+        System.out.println(" (at: " + byDate + ")" );
+    }
+
+    public void printAction(){
+        super.printAction();
+        printStatus();
+        System.out.println("Now you have "+ TaskCount + " tasks in the list.");
+    }
 }
